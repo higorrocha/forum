fb3f03638368001ce6903a50ee825fb7246b6dc8
higorrocha <higorrocha.14@gmail.com>
2016-07-28 11:33:55 -0300

    Add association between post and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728141359) do
+ActiveRecord::Schema.define(version: 20160728142259) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160728141359) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
